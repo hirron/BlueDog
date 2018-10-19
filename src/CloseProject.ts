7d500f6f7b35045c1a6c2e05a331651cdd822608
Devlin Wyatt <devlinwyatt@gmail.com>
2018-10-19 12:37:52 -0700

    Create CloseProject.ts

---
+++ src/CloseProject.ts
@@ -0,0 +1,20 @@
+class CloseProject {
+	userName: string;
+	
+  constructor(public userName: string, public pwd: string){
+  }
+}
+
+interface User {
+	userName: string;
+	pwd: string;
+}
+
+function Permission(user : User){
+  if(user.pwd == database.userPwd) return true;
+  else return false;
+}
+
+let user = new User("Admin", "1234567890");
+
+document.body.innerHTML = Permission(user);
